c03f966a802391ce75fd7cb060c86db6223a8225
Oluwafemi <oluwafemiomolounnu@gmail.com>
2023-11-17 20:38:49 +0100

    done with styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 
 
 const pizzaData = [
@@ -57,6 +57,7 @@ function App() {
   );
 }
 
+
 function Header() {
   return (
     <header className='header'>
@@ -64,26 +65,73 @@ function Header() {
     </header>
   )
 }
-function Pizza() {
-  return (
-    <div className="App">
-      <img src='/pizzas/spinaci.jpg' alt='pizza spinaci'/>
-      <h3>{pizzaData[0].name}</h3>
-    </div>
-  );
-}
 
 
 const Menu = () =>{
   return (
     <main className='menu'>
       <h2>Menu</h2>
-    <Pizza/>
-    <Pizza/>
-    <Pizza/>
+    <div className='pizzas'>
+    <Pizza 
+    name={pizzaData[0].name}
+    ingredients={pizzaData[0].ingredients}
+    price={pizzaData[0].price}
+    photoName={pizzaData[0].photoName}
+    soldOut={pizzaData[0].soldOut}
+    />
+    <Pizza
+     name={pizzaData[1].name}
+     ingredients={pizzaData[1].ingredients}
+     price={pizzaData[1].price}
+     photoName={pizzaData[1].photoName}
+     soldOut={pizzaData[1].soldOut}
+    />
+    <Pizza
+     name={pizzaData[2].name}
+     ingredients={pizzaData[2].ingredients}
+     price={pizzaData[2].price}
+     photoName={pizzaData[2].photoName}
+     soldOut={pizzaData[2].soldOut}
+    />
+      <Pizza
+     name={pizzaData[3].name}
+     ingredients={pizzaData[3].ingredients}
+     price={pizzaData[3].price}
+     photoName={pizzaData[3].photoName}
+     soldOut={pizzaData[3].soldOut}
+    />
+      <Pizza
+     name={pizzaData[4].name}
+     ingredients={pizzaData[4].ingredients}
+     price={pizzaData[4].price}
+     photoName={pizzaData[4].photoName}
+     soldOut={pizzaData[4].soldOut}
+    />
+      <Pizza
+     name={pizzaData[5].name}
+     ingredients={pizzaData[5].ingredients}
+     price={pizzaData[5].price}
+     photoName={pizzaData[5].photoName}
+     soldOut={pizzaData[5].soldOut}
+    />
+    </div>
     </main>
   );
 };
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}/>
+      <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price}</span>
+      <span>{props.soldOut}</span>
+      </div>
+    </div>
+  );
+}
 
 function Footer() {
   const hour = new Date().getHours();
